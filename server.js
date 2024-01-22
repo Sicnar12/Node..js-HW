d01e3d67433e86d6e856555600f516ae18878e18
unknown <adetutu.francisb@gmail.com>
2024-01-22 11:25:16 -0500

    update

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require ('fs')
+
+ const server = http.createServer((req, res) =>{
+console.log(req.url, req.method);
+
+// set header content type
+res.setHeader('Content-Type', 'text/html');
+
+let path = './views/';
+switch(req.url) {
+    case '/':
+        path += 'index.html'
+        res.statusCode = 200;
+        break;
+        case '/about':
+        path += 'about.html';
+        res.statusCode = 200
+        break;
+         case '/about-me':
+         res.statusCode = 301;
+         res.setHeader('Location', '/about');
+         res.end();
+        default:
+            path+= '404.html'
+            res.statusCode = 404;
+            break;
+}
+
+// send an html file
+fs.readFile(path, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        // res.write(data);
+        res.statusCode = 200;
+        res.end(data);
+    }
+})
+
+// res.write('<head><link rel="stylesheet"href= "#"></head>');
+// res.write('<p>hello, ningas</p>');
+// res.write('<p>hello again, ningas</p>');
+// res.end();
+ });
+ server.listen(3000, 'localhost', () => {
+    console.log('listening on request on port 3000');
+ });
