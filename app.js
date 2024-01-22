7679d1da745ab1eec677ee9dd1c53f7926a18ad2
unknown <adetutu.francisb@gmail.com>
2024-01-21 22:56:05 -0500

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+const name = 'adetutu';
+
+console.log(name); 
+
+const greet =(name => {
+    console.log(`hello', ${name}`);
+})
+greet('adetutu')
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['yoshi', 'carla', 'carmen', 'mario'];
+const age = [23,26.60,50];
+
+console.log(people);
+modules.export = {
+    people, ages
+};
