9732f3ef1e098073963f8fa383514ff59a03b97e
unknown <adetutu.francisb@gmail.com>
2024-02-05 16:17:51 -0500

    update

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+
 //listen for request
 app.listen(3000);
 
+//middleware and static files
+app.use(express.static('public'))
+ 
+app.use(morgan('dev'));
+ 
+// routes
 app.get('/', (req, res)=> {
+    res.redirect('')
 
 // res.send('<p>home page </p>');
-res.sendFile('./views/index.html', {root: __dirname});
+res.render('index', {title: 'Home', blogs});
 });
 app.get('/about', (req, res)=> {
     // res.send('<p>about page </p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 
 });
+// blog routes
+app.get('/blogs')
 
 //redirects
-app.get('/about-us', (req, res)=> {
-    res.redirect('/about');
-});
+// app.get('/about-us', (req, res)=> {
+//     res.redirect('/about');
+// });
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'create a new blog'});
+})
 
 //404 page
 app.use((req, res)=>{
-    res.status(404).sendFile('/views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404'});
 });
